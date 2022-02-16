f8ede3791e92d592399d8b908f863c78634cdca6
raja.m <raja.m@heymath.com>
2022-02-16 22:33:25 +0530

    Add Button Function

--- app/src/main/java/com/heymath/kotlinlearn/MainActivity.kt
+++ app/src/main/java/com/heymath/kotlinlearn/MainActivity.kt
@@ -2,11 +2,20 @@ package com.heymath.kotlinlearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+//   Github password token(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)
+//    https://www.androhub.com/working-with-git-in-android-studio/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button =findViewById<Button>(R.id.clickMe)
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Its First function",Toast.LENGTH_SHORT).show()
+        }
     }
 }
