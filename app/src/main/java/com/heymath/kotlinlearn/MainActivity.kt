ee969d391a27c82cc695b479633eecfd8ac0c399
raja.m <raja.m@heymath.com>
2022-02-18 12:20:31 +0530

    check permission

--- app/src/main/java/com/heymath/kotlinlearn/MainActivity.kt
+++ app/src/main/java/com/heymath/kotlinlearn/MainActivity.kt
@@ -1,21 +1,93 @@
 package com.heymath.kotlinlearn
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-//   Github password token(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)
+    //   Github password token(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)
 //    https://www.androhub.com/working-with-git-in-android-studio/
+//    https://www.javatpoint.com/kotlin-for-loop
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button =findViewById<Button>(R.id.clickMe)
+        val button = findViewById<Button>(R.id.clickMe)
+
         button.setOnClickListener {
-            Toast.makeText(this@MainActivity,"Its First function",Toast.LENGTH_SHORT).show()
+            onClickRequestPermission(button)
+        }
+
+
+        fun View.showSnackbar(
+            view: View,
+            msg: String,
+            length: Int,
+            actionMessage: CharSequence?,
+            action: (View) -> Unit
+        ) {
+            val snackbar_view = Snackbar.make(view, msg, length)
+            if (actionMessage != null) {
+                snackbar_view.setAction(actionMessage) {
+                    action(this)
+                }.show()
+            } else {
+                snackbar_view.show()
+            }
+        }
+
+    }
+
+    private fun onClickRequestPermission(button: Button?) {
+        when {
+            ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.permission_granted),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this@MainActivity,
+                Manifest.permission.CAMERA
+            ) -> {
+
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.permission_required),
+                    Toast.LENGTH_SHORT
+                ).show()
+                requestPermission.launch(Manifest.permission.CAMERA)
+            }
+            else -> {
+                requestPermission.launch(Manifest.permission.CAMERA)
+            }
+
         }
     }
+
 }
